705ffc4a9667ae7d4f7b236138ba61233fd4f3f5
=Zidane KABI <zidane.kabi@gmail.com>
2025-06-22 18:45:33 +0200

    debut liste employe

--- Frontend/front-paie/src/app/employes/employes/employes.component.ts
+++ Frontend/front-paie/src/app/employes/employes/employes.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 @Component({
   selector: 'app-employes',
-  imports: [],
+  imports: [MatCardModule,
+           MatTableModule
+  ],
   templateUrl: './employes.component.html',
   styleUrl: './employes.component.css'
 })
-export class EmployesComponent {
+export class EmployesComponent implements OnInit {
+
+  private readonly http = inject(HttpClient);
+  dataSource: any  ;
+  public displayedColumns  : string[] = ['nom', 'prenom', 'email', 'categorie', 'salairebase'];
+  
+
+
+  ngOnInit(): void {
+      
+  }
+
+
 
 }
